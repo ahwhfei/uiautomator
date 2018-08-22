a5ef1060ad4c7f96eb1e369f4dde7a1ac1700ce8
Hongfei Wu <hongfei.wu@citrix.com>
2018-08-22 16:09:17 +0800

    Add element evaluate before click

--- demo.js
+++ demo.js
@@ -68,6 +68,41 @@ const clickXPath = async (xpath, options, extraTimeout, hasNavigation) => {
     }
 };
 
+const clickEvaluate = async (selector, extraTimeout, hidden, hasNavigation) => {
+    let waitForNavigationPromise;
+
+    try {
+        if (extraTimeout && typeof extraTimeout === 'number') {
+            await page.waitFor(extraTimeout);
+        }
+
+        const hasElement = await page.evaluate((s) => !!document.querySelector(s), selector);
+        if (!hasElement) {
+            return hasElement;
+        }
+
+        if (hasNavigation) {
+            waitForNavigationPromise = page.waitForNavigation();
+        }
+
+        await page.click(selector);
+
+        if (hidden) {
+            await page.waitForSelector(selector, {hidden: true});
+        }
+
+        if (hasNavigation) {
+            await waitForNavigationPromise;
+        }
+
+        return hasElement;
+    } catch(err) {
+        console.error(`ClickEvaluate Selector: "${selector}" Exception!`);
+        console.error(err);
+        throw new Error('Exist Exception! STOPPED!!!');
+    }
+};
+
 const run = async () => {
     const width = 1400;
     const height = 800;
@@ -95,9 +130,17 @@ const run = async () => {
     
     const SIGNIN_SELECTOR = '#submit';
     await clickSelector(SIGNIN_SELECTOR, {visible: true, timeout: timeout}, 0, true);
+
+    const MULTIPLE_CUSTOMERS_SELECTOR = 'body > div.ng-isolate-scope.cwc-isolate-navbar > div > div > div > div > div > div > div > div';
+    const hasMultipleCustomers = await clickEvaluate(MULTIPLE_CUSTOMERS_SELECTOR, 1000);
     
-    const SELECT_CUSTOMER_SELECTOR = 'body > div.ng-isolate-scope.cwc-isolate-navbar > div > div > div > div > div > div > ctx-scroll-select > div > div > ctx-scroll-select-item:nth-child(10) > div > span';
-    await clickSelector(SELECT_CUSTOMER_SELECTOR, undefined, 0, true);
+    if (hasMultipleCustomers) {
+        const SELECT_CUSTOMER_SELECTOR = 'body > div.ng-isolate-scope.cwc-isolate-navbar > div > div > div > div > div > div > ctx-scroll-select > div > div > ctx-scroll-select-item:nth-child(10) > div > span';
+        await clickSelector(SELECT_CUSTOMER_SELECTOR, undefined, 0, true);
+    }
+
+    const PENDO_WELCOME_DIALOG_SELECTOR = '#_pendo-close-guide_';
+    await clickEvaluate(PENDO_WELCOME_DIALOG_SELECTOR, 2000, true);
     
     const MANAGE_SELECTOR = 'body > ctx-app > div > ctx-shell > div > ctx-dashboard > div > ctx-dashboard-services > div > div.ctx-dashboard-services-my-services.ng-star-inserted > ctx-dashboard-services-group > div > div.ctx-dashboard-services-group-content > ctxgrid > div > div:nth-child(12) > ctxgridtile > div > div > ctx-dashboard-services-tile > div > div.ctx-dashboard-services-tile-inner > div.ctx-dashboard-services-tile-inner-button.ng-star-inserted > ctx-spinner-2 > div > div';
     await clickSelector(MANAGE_SELECTOR, {visible: true, timeout: timeout}, 1000, true);
@@ -106,15 +149,8 @@ const run = async () => {
     await clickSelector(XA_MANAGE_SELECTOR, {visible: true, timeout: timeout});
 
     // TODO: Wait 20s for HTML5 receiver session setup
-    await page.waitFor(20000);
-
     const CONTINUE_TO_FULL_CONFIGURATION = 'body > modal-container > div > div > ng-component > div.modal-footer.pull-left > button.ctx-button.ml-0';
-    const hasPendoDialog = await page.evaluate((selector) => !!document.querySelector(selector), CONTINUE_TO_FULL_CONFIGURATION);
-    if (hasPendoDialog) {
-        await page.click(CONTINUE_TO_FULL_CONFIGURATION);
-    }
-
-    await page.waitForSelector(CONTINUE_TO_FULL_CONFIGURATION, {hidden: true});
+    await clickEvaluate(CONTINUE_TO_FULL_CONFIGURATION, 20000, true);
 
     const MENU_SELECTOR = '#username-admin-display-name > div.username-row.admin-name.ng-binding';
     await clickSelector(MENU_SELECTOR, {visible: true, timeout: timeout}, 3000);
