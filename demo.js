341a85e675cbb5ad59d7b112d502f37acc120273
Hongfei Wu <hongfei.wu@citrix.com>
2018-08-22 14:47:56 +0800

    Add clickSelector and clickXpath functions

--- demo.js
+++ demo.js
@@ -1,6 +1,73 @@
 const puppeteer = require('puppeteer');
 const CREDS = require('./creds');
 
+let page;
+
+const clickSelector = async (selector, options, extraTimeout, hasNavigation) => {
+    let waitForNavigationPromise;
+
+    try {
+        if (typeof options === 'object') {
+            await page.waitFor(selector, options);
+        } else {
+            await page.waitFor(selector);
+        }
+
+        if (extraTimeout && typeof extraTimeout === 'number') {
+            await page.waitFor(extraTimeout);
+        }
+
+        if (hasNavigation) {
+            waitForNavigationPromise = page.waitForNavigation();
+        }
+
+        await page.click(selector);
+
+        if (hasNavigation) {
+            await waitForNavigationPromise;
+        }
+    } catch(err) {
+        console.error(`Click Selector: "${selector}" Exception!`);
+        console.error(err);
+        throw new Error('Exist Exception! STOPPED!!!');
+    }
+};
+
+const clickXPath = async (xpath, options, extraTimeout, hasNavigation) => {
+    let waitForNavigationPromise;
+
+    try {
+        if (typeof options === 'object') {
+            await page.waitForXPath(xpath, options);
+        } else {
+            await page.waitForXPath(selector);
+        }
+
+        if (extraTimeout && typeof extraTimeout === 'number') {
+            await page.waitFor(extraTimeout);
+        }
+
+        if (hasNavigation) {
+            waitForNavigationPromise = page.waitForNavigation();
+        }
+
+        const element = await page.$x(xpath);
+        if (element && element.length === 1) {
+            await element[0].click();
+        } else {
+            throw new Error('element not found');
+        }
+
+        if (hasNavigation) {
+            await waitForNavigationPromise;
+        }
+    } catch(err) {
+        console.error(`Click XPath: "${selector}" Exception!`);
+        console.error(err);
+        throw new Error('Exist Exception! STOPPED!!!');
+    }
+};
+
 const run = async () => {
     const width = 1400;
     const height = 800;
@@ -11,42 +78,34 @@ const run = async () => {
             `--window-size=${width},${height}`
         ]
     });
-    const page = await browser.newPage();
+    page = await browser.newPage();
     await page.setViewport({ width, height });
     await page.setDefaultNavigationTimeout(timeout);
     await page.goto('https://citrix.cloud.com');
 
     const USERNAME_SELECTOR = '#username';
     const PASSWORD_SELECTOR = '#password';
-    const SIGNIN_SELECTOR = '#submit';
-
+    
     await page.waitFor(USERNAME_SELECTOR, {visible: true, timeout: timeout});
     await page.click(USERNAME_SELECTOR);
     await page.keyboard.type(CREDS.username);
-
+    
     await page.click(PASSWORD_SELECTOR);
     await page.keyboard.type(CREDS.password);
     
-    let waitForNavigationPromise = page.waitForNavigation();
-    await page.click(SIGNIN_SELECTOR);
-    await waitForNavigationPromise;
+    const SIGNIN_SELECTOR = '#submit';
+    await clickSelector(SIGNIN_SELECTOR, {visible: true, timeout: timeout}, 0, true);
     
     const SELECT_CUSTOMER_SELECTOR = 'body > div.ng-isolate-scope.cwc-isolate-navbar > div > div > div > div > div > div > ctx-scroll-select > div > div > ctx-scroll-select-item:nth-child(10) > div > span';
-    await page.waitFor(SELECT_CUSTOMER_SELECTOR);
-    waitForNavigationPromise = page.waitForNavigation();
-    await page.click(SELECT_CUSTOMER_SELECTOR);
-    await waitForNavigationPromise;    
+    await clickSelector(SELECT_CUSTOMER_SELECTOR, undefined, 0, true);
     
     const MANAGE_SELECTOR = 'body > ctx-app > div > ctx-shell > div > ctx-dashboard > div > ctx-dashboard-services > div > div.ctx-dashboard-services-my-services.ng-star-inserted > ctx-dashboard-services-group > div > div.ctx-dashboard-services-group-content > ctxgrid > div > div:nth-child(12) > ctxgridtile > div > div > ctx-dashboard-services-tile > div > div.ctx-dashboard-services-tile-inner > div.ctx-dashboard-services-tile-inner-button.ng-star-inserted > ctx-spinner-2 > div > div';
-    await page.waitFor(MANAGE_SELECTOR, {visible: true, timeout: timeout});
-    waitForNavigationPromise = page.waitForNavigation();
-    await page.click(MANAGE_SELECTOR);
-    await waitForNavigationPromise;
+    await clickSelector(MANAGE_SELECTOR, {visible: true, timeout: timeout}, 1000, true);
 
     const XA_MANAGE_SELECTOR = 'body > app > navbar > div > div > ul.xd-navbar-left.xd-navbar-color-light > li:nth-child(2) > a.ng-tns-c1-0.has-dropdown';
-    await page.waitFor(XA_MANAGE_SELECTOR, {visible: true, timeout: timeout});
-    await page.click(XA_MANAGE_SELECTOR);
+    await clickSelector(XA_MANAGE_SELECTOR, {visible: true, timeout: timeout});
 
+    // TODO: Wait 20s for HTML5 receiver session setup
     await page.waitFor(20000);
 
     const CONTINUE_TO_FULL_CONFIGURATION = 'body > modal-container > div > div > ng-component > div.modal-footer.pull-left > button.ctx-button.ml-0';
@@ -56,30 +115,22 @@ const run = async () => {
     }
 
     await page.waitForSelector(CONTINUE_TO_FULL_CONFIGURATION, {hidden: true});
-    await page.waitFor(5000);
 
     const MENU_SELECTOR = '#username-admin-display-name > div.username-row.admin-name.ng-binding';
-    await page.waitFor(MENU_SELECTOR, {visible: true, timeout: timeout});
-    await page.click(MENU_SELECTOR);
+    await clickSelector(MENU_SELECTOR, {visible: true, timeout: timeout}, 3000);
 
-    // const LOGOFF_SELECTOR = 'body > cwc-navbar > div > div > div.pull-right > div.username-container.vmid.XenApp.and.XenDesktop.Service > div > div > div > div.cwc-dropdown-menu.username-menu.XenApp.and.XenDesktop.Service > div:nth-child(4) > a > span';
-    const LOGOFF_SELECTOR = '//body/cwc-navbar/div/div/div[4]/div[2]/div/div/div/div[2]/div[4]/a/span[.="Sign Out"]';
-    await page.waitFor(LOGOFF_SELECTOR, {visible: true, timeout: timeout});
+    const LOGOFF_SELECTOR = 'body > cwc-navbar > div > div > div.pull-right > div.username-container.vmid.XenApp.and.XenDesktop.Service > div > div > div > div.cwc-dropdown-menu.username-menu.XenApp.and.XenDesktop.Service > div:nth-child(4) > a > span';
+    await clickSelector(LOGOFF_SELECTOR, {visible: true, timeout}, 1000, true);
 
-    await page.waitFor(1000);
-    const logOffElement = await page.$x(LOGOFF_SELECTOR);
-    if (logOffElement.length === 1) {
-        await logOffElement[0].click();
-    } else {
-        throw new Error('element not found');
-    }
+    // const LOGOFF_SELECTOR = '//body/cwc-navbar/div/div/div[4]/div[2]/div/div/div/div[2]/div[4]/a/span[.="Sign Out"]';
+    // await clickXPath(LOGOFF_SELECTOR, {visible: true, timeout}, 1000, true);
 
-    await page.waitFor(10000);
+    await page.waitFor(5000);
     await browser.close();
 };
 
 
-const COUNT = 2;
+const COUNT = 1;
 for (let i = 0; i < COUNT; i++) {
     run();
 }
