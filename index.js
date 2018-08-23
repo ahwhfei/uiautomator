fcf947b8539287536a42e62841bed53aacd9d545
Hongfei Wu <hongfei.wu@citrix.com>
2018-08-23 20:30:38 +0800

    Add args

--- index.js
+++ index.js
@@ -1,25 +1,58 @@
 #!/usr/bin/env node
 
-const ActionExecution = require('./action-execution');
+const fs = require('fs');
 const TMActionExecution = require('./tm-action-execution');
-const creds = require('./data/customers');
 
-const actionList2 = require('./data/actions-2');
-const actionList8_qiandev32 = require('./data/actions-8-qiandev32');
-const actionList8_qiandev33 = require('./data/actions-8-qiandev33');
+const tmActionList = require('./data/tm-actions.json');
 
-const actionList_xpath = require('./data/actions-xpath.json').data;
-const tmActionList = require('./data/tm-actions.json').data;
+let help = false;
+let testSequenceFile;
+let parallelCount = 1;
+let rawContent;
+let actions;
+let creds;
+const args = process.argv.slice(2).filter((arg) => {
+    if (arg.match(/^(-+|\/)(h(elp)?|\?)$/)) {
+        help = true;
+    } else if (arg.indexOf('--file') === 0 || arg.indexOf('-f') === 0) {
+        testSequenceFile = arg.split('=')[1];
+    } else if (arg.indexOf('--parallel') === 0 || arg.indexOf('-p') === 0) {
+        parallelCount = +arg.split('=')[1];
+    }
+});
 
-const execution = new ActionExecution();
-// execution.run(actionList2);
-// execution.run(actionList8_qiandev32);
-// execution.run(actionList8_qiandev33);
-// execution.run(actionList_xpath);
+if (help) {
+    // If they didn't ask for help, then this is not a "success"
+    var log = help ? console.log : console.error
+    log('Usage: uiautomator [<InputFile> <ParallelCount>]')
+    log('')
+    log('  UI automation tool.')
+    log('')
+    log('Options:')
+    log('')
+    log('  -f, --file         Test sequence json file')
+    log('  -p, --parallel     Parallel count')
+    process.exit(help ? 0 : 1)
+} else if (testSequenceFile) {
+    try {
+        rawContent = fs.readFileSync(testSequenceFile, 'utf8');
+        const content = JSON.parse(rawContent);
+        actions = content.data;
+        creds = content.creds;
+    } catch (err) {
+        console.log(err.message);
+    }
+}
 
-const tmExecution1 = new TMActionExecution(tmActionList, creds);
-tmExecution1.run();
-const tmExecution2 = new TMActionExecution(tmActionList, creds);
-tmExecution2.run();
-const tmExecution3 = new TMActionExecution(tmActionList, creds);
-tmExecution3.run();
+if (!actions) {
+    actions = tmActionList.data;
+}
+
+if (!creds) {
+    creds = tmActionList.creds;
+}
+
+for (let i = 0; i < parallelCount; i++) {
+    const tmExecution = new TMActionExecution(actions, creds);
+    tmExecution.run();
+}
