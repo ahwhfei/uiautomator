78f9d17a9434232034692ac451785450168d0810
Hongfei Wu <hongfei.wu@citrix.com>
2018-08-23 17:22:07 +0800

    Move test data to data folder

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const ActionExecution = require('./action-execution');
 const TMActionExecution = require('./tm-action-execution');
 
-const actionList2 = require('./actions-2');
-const actionList8_qiandev32 = require('./actions-8-qiandev32');
-const actionList8_qiandev33 = require('./actions-8-qiandev33');
+const actionList2 = require('./data/actions-2');
+const actionList8_qiandev32 = require('./data/actions-8-qiandev32');
+const actionList8_qiandev33 = require('./data/actions-8-qiandev33');
 
-const actionList_xpath = require('./actions-xpath.json').data;
+const actionList_xpath = require('./data/actions-xpath.json').data;
 const tmActionList = require('./data/tm-actions.json').data;
 
 const execution = new ActionExecution();
